67532ce80f89fdc32543557d40dcd25c1f5d180e
DefneEkin <116458662+DefneEkin@users.noreply.github.com>
2025-02-17 23:15:34 +0300

    Update VideoProcessor.hpp

---
+++ VideoProcessor.hpp
@@ -0,0 +1,22 @@
+#ifndef VIDEO_PROCESSOR_HPP
+#define VIDEO_PROCESSOR_HPP
+
+#include "YOLODetector.hpp"
+#include "CarTracker.hpp"
+#include <opencv2/opencv.hpp>
+#include <vector>
+
+class VideoProcessor {
+public:
+    VideoProcessor(const string & video_path, const string & output_path, YOLODetector & detector, CarTracker & tracker);
+    void process();
+
+private:
+    string video_path;
+    string output_path;
+    YOLODetector & detector;
+    CarTracker & tracker;
+    vector<Car> car_vector;
+};
+
+#endif
